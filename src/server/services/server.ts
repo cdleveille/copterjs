27280eb3acfde363d5ef28534e7fcc78a2abf5a4
Chris Leveille <cdleveille@gmail.com>
2022-04-07 15:48:47 -0400

    csp violation fixes

--- src/server/services/server.ts
+++ src/server/services/server.ts
@@ -42,7 +42,8 @@ export default class App {
 					],
 					"style-src": ["'self'", "'unsafe-inline'", "cdnjs.cloudflare.com", "fonts.googleapis.com"],
 					"font-src": ["'self'", "'unsafe-inline'", "fonts.googleapis.com", "fonts.gstatic.com"],
-					"img-src": ["'self' blob: data:"]
+					"img-src": ["'self' blob: data:"],
+					"connect-src": ["'self'", "googletagmanager.com"]
 				}
 			})
 		);
