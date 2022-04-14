0be2c1d68c2a857145ffb7f41116c2812e079ffa
Chris Leveille <cdleveille@gmail.com>
2022-04-14 06:00:32 -0400

    script cleanup and package trimming

--- src/shared/types/abstract.ts
+++ src/shared/types/abstract.ts
@@ -3,24 +3,6 @@ export interface IScore {
 	score: number;
 }
 
-// export interface ISocket extends ISocketBroadcast {
-// 	id: string;
-// 	connected: boolean;
-// 	disconnected: boolean;
-// 	connect: (params: ISocketParams) => ISocket;
-// 	on: (listener: string, callback: (...params: any[]) => any) => void;
-// 	broadcast: ISocketBroadcast;
-// }
-
-// interface ISocketBroadcast {
-// 	emit: (listener: string, data?: any) => void;
-// }
-
-// interface ISocketParams {
-// 	reconnectionDelay: number;
-// 	reconnectionAttempts: number;
-// }
-
 export interface IEnvVars {
 	IS_PROD: boolean;
 	USE_DB: boolean;
